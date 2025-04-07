1423d051374edec6d420ac94650710c58d5f8a2a
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-04-07 19:23:56 +0100

    add home controller

--- CodeSource/app/Http/Controllers/HomeController.php
+++ CodeSource/app/Http/Controllers/HomeController.php
@@ -12,10 +12,10 @@ class HomeController extends Controller
 
     public function index()
     {
-        // Get posts from users that the authenticated user follows
+      
         $followingIds = auth()->user()->following()->pluck('users.id');
         
-        // Include the user's own posts
+        
         $followingIds[] = auth()->id();
         
         $posts = Post::whereIn('user_id', $followingIds)
@@ -23,14 +23,14 @@ class HomeController extends Controller
             ->latest()
             ->paginate(10);
             
-        // Get trending posts (most liked in the last week)
+        
         $trendingPosts = Post::withCount('likes')
             ->where('created_at', '>=', now()->subWeek())
             ->orderBy('likes_count', 'desc')
             ->take(3)
             ->get();
             
-        // Get users that the authenticated user might know (not following yet)
+       
         $suggestedUsers = User::whereNotIn('id', $followingIds)
             ->where('id', '!=', auth()->id())
             ->inRandomOrder()
