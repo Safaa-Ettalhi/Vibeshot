994307cf9ec1ed2e78c4f68a8fb8523bf15ceea9
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-04-08 09:37:56 +0100

    add model like

--- CodeSource/app/Models/Post.php
+++ CodeSource/app/Models/Post.php
@@ -12,7 +12,7 @@ class Post extends Model
     protected $fillable = [
         'user_id',
         'caption',
-        'image_path', // Gardé pour compatibilité avec l'existant
+        'image_path',
     ];
 
     public function user()
