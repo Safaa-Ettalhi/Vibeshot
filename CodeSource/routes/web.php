618e7d0283ca519cbd14f1112611b3ef6db230f0
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-04-12 21:04:07 +0100

    add route for users

--- CodeSource/routes/web.php
+++ CodeSource/routes/web.php
@@ -1,7 +1,72 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\BookmarkController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\FollowController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+});
+
+// Routes d'authentification
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+});
+
+
+Route::middleware('auth')->group(function () {
+    // Déconnexion
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+    
+    // Page d'accueil
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    
+    // Posts
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    
+    // Commentaires
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+    
+    // Likes
+    Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('likes.store');
+    Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('likes.destroy');
+    
+    // Bookmarks
+    Route::get('/bookmarks', [BookmarkController::class, 'index'])->name('bookmarks.index');
+    Route::post('/posts/{post}/bookmarks', [BookmarkController::class, 'store'])->name('bookmarks.store');
+    Route::delete('/posts/{post}/bookmarks', [BookmarkController::class, 'destroy'])->name('bookmarks.destroy');
+    
+    // Profil
+    Route::get('/profile/{username}', [ProfileController::class, 'show'])->name('profile.show');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
+    
+    // Abonnements
+    Route::post('/users/{user}/follow', [FollowController::class, 'store'])->name('follow.store');
+    Route::delete('/users/{user}/follow', [FollowController::class, 'destroy'])->name('follow.destroy');
+    
+    // Notifications
+    Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
+    Route::post('/notifications/{id}/read', [NotificationController::class, 'markAsRead'])->name('notifications.read');
+    Route::post('/notifications/read-all', [NotificationController::class, 'markAllAsRead'])->name('notifications.read.all');
+    
+    // Recherche
+    Route::get('/search', [SearchController::class, 'index'])->name('search');
 });
