21ab0048cb7406fec0ebe6658380c7ea4eb55e0a
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-04-23 17:03:22 +0100

    modification de table notification

--- CodeSource/database/migrations/2025_04_07_092007_create_notification_table.php
+++ CodeSource/database/migrations/2025_04_07_092007_create_notification_table.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,15 +9,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('notifiable'); // Ajoute `notifiable_type` et `notifiable_id`
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('post_id')->nullable()->constrained()->onDelete('cascade');
-            $table->string('type'); // 'like', 'comment', 'follow'
-            $table->boolean('is_read')->default(false);
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
             $table->timestamp('read_at')->nullable();
-            $table->json('data')->nullable(); // Ajout de la colonne 'data' pour stocker les informations JSON
             $table->timestamps();
         });
     }
