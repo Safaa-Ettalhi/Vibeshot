61e803b46e7a20fc1841efa0777d2caa582508cf
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-04-23 21:50:02 +0100

    ajout d'une notification pour les nouveaux commentaires sur un post

--- CodeSource/app/Notifications/NewCommentNotification.php
+++ CodeSource/app/Notifications/NewCommentNotification.php
@@ -4,13 +4,14 @@ namespace App\Notifications;
 
 use App\Models\Comment;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\BroadcastMessage;
 
-class NewCommentNotification extends Notification implements ShouldQueue
+class NewCommentNotification extends Notification
 {
     use Queueable;
 
@@ -24,37 +25,34 @@ class NewCommentNotification extends Notification implements ShouldQueue
         $this->comment = $comment;
     }
 
-  
     public function via(object $notifiable): array
     {
-        return ['database', 'broadcast'];
+        return ['database'];
     }
 
     public function toArray(object $notifiable): array
     {
         return [
-            'user_id' => $this->comment->user->id,
-            'user_name' => $this->comment->user->name,
-            'user_username' => $this->comment->user->username,
-            'user_image' => $this->comment->user->profile_image,
+            'user_id' => auth()->user()->id,
+            'user_name' => auth()->user()->name,
+            'user_username' => auth()->user()->username,
+            'user_image' => auth()->user()->profile_image,
             'post_id' => $this->post->id,
             'comment_id' => $this->comment->id,
-            'comment_content' => $this->comment->content,
             'message' => 'a commenté votre publication',
             'type' => 'comment',
         ];
     }
-
+  
     public function toBroadcast($notifiable)
     {
         return new BroadcastMessage([
-            'user_id' => $this->comment->user->id,
-            'user_name' => $this->comment->user->name,
-            'user_username' => $this->comment->user->username,
-            'user_image' => $this->comment->user->profile_image,
+            'user_id' => auth()->user()->id,
+            'user_name' => auth()->user()->name,
+            'user_username' => auth()->user()->username,
+            'user_image' => auth()->user()->profile_image,
             'post_id' => $this->post->id,
             'comment_id' => $this->comment->id,
-            'comment_content' => $this->comment->content,
             'message' => 'a commenté votre publication',
             'type' => 'comment',
             'created_at' => now()->diffForHumans(),
