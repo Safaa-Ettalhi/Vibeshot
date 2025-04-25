4d6267b4e805d5b2f4000ca27da4df8ef8050aac
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-04-25 09:30:30 +0100

    ajout de la recherche de posts

--- CodeSource/app/Http/Controllers/HomeController.php
+++ CodeSource/app/Http/Controllers/HomeController.php
@@ -8,23 +8,30 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-
-
-    public function index()
+    public function index(Request $request)
     {
-      
+        $query = $request->input('search');
         $followingIds = auth()->user()->following()->pluck('users.id');
-        
-        
         $followingIds[] = auth()->id();
         
-        $posts = Post::whereIn('user_id', $followingIds)
+        $postsQuery = Post::whereIn('user_id', $followingIds)
             ->where('is_hidden', false)
-            ->with(['user', 'comments.user', 'likes'])
-            ->latest()
-            ->get(); 
+            ->with(['user', 'comments.user', 'likes', 'images']);
+        $searchPerformed = false;
 
-            
+        if ($query) {
+            $searchPerformed = true;
+            $postsQuery = $postsQuery->where(function($q) use ($query) {
+                $q->where('caption', 'LIKE', "%{$query}%")
+                  ->orWhereHas('user', function($userQuery) use ($query) {
+                      $userQuery->where('name', 'LIKE', "%{$query}%")
+                                ->orWhere('username', 'LIKE', "%{$query}%");
+                  });
+            });
+        }
+        
+        $posts = $postsQuery->latest()->get();
+        $resultsCount = $posts->count();
         
         $trendingPosts = Post::withCount('likes')
             ->where('created_at', '>=', now()->subWeek())
@@ -32,15 +39,14 @@ class HomeController extends Controller
             ->orderBy('likes_count', 'desc')
             ->take(3)
             ->get();
-            
-       
+        
         $suggestedUsers = User::whereNotIn('id', $followingIds)
             ->where('id', '!=', auth()->id())
             ->where('is_blocked', false)
             ->inRandomOrder()
             ->take(5)
             ->get();
-            
-        return view('home', compact('posts', 'trendingPosts', 'suggestedUsers'));
+        
+        return view('home', compact('posts', 'trendingPosts', 'suggestedUsers', 'query', 'resultsCount', 'searchPerformed'));
     }
 }
