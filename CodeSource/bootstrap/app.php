2c7a99163b6ed8bc0dfe03b56e995e547a509eb7
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-04-27 12:32:21 +0100

    Ajouter l'alias du middleware RedirectIfAdmin

--- CodeSource/bootstrap/app.php
+++ CodeSource/bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
         'admin' => \App\Http\Middleware\IsAdmin::class,
+        'not.admin' => \App\Http\Middleware\RedirectIfAdmin::class,
     ]);
         $middleware->web(append: [
             \App\Http\Middleware\CheckUserIsNotBlocked::class,
