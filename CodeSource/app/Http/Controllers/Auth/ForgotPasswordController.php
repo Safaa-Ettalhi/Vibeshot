a2cafae12a3bd46665319716d62331078dfae0dc
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-04-30 18:12:15 +0100

    Ajout du controleur ForgotPasswordController pour la réinitialisation de mot de passe

---
+++ CodeSource/app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
+
+class ForgotPasswordController extends Controller
+{
+    public function showLinkRequestForm()
+    {
+        return view('auth.passwords.email');
+    }
+
+    public function sendResetLinkEmail(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+        ]);
+
+        if ($validator->fails()) {
+            return back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::RESET_LINK_SENT
+            ? back()->with('status', 'A reset link has been sent to your email address.')
+            : back()->withErrors(['email' => __($status)]);
+    }
+}
