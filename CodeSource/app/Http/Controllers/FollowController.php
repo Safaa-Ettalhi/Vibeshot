cc6b73c86996ef14e6d074f6b49925644d641708
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-05-02 14:23:08 +0100

    Ajout d'une condition pour détecter les requêtes AJAX

--- CodeSource/app/Http/Controllers/FollowController.php
+++ CodeSource/app/Http/Controllers/FollowController.php
@@ -8,23 +8,39 @@ use Illuminate\Http\Request;
 
 class FollowController extends Controller
 {
-    
     public function store(User $user)
     {
-       
         if (auth()->id() === $user->id) {
+            if (request()->ajax()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Vous ne pouvez pas vous suivre vous-même'
+                ]);
+            }
             return redirect()->back();
         }
         
-      
         if (auth()->user()->following()->where('following_id', $user->id)->exists()) {
+            if (request()->ajax()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Vous suivez déjà cet utilisateur'
+                ]);
+            }
             return redirect()->back();
         }
         
         auth()->user()->following()->attach($user->id);
         
-      
         $user->notify(new NewFollowerNotification(auth()->user()));
+        
+        if (request()->ajax()) {
+            return response()->json([
+                'success' => true,
+                'following' => true,
+                'followers_count' => $user->followers()->count()
+            ]);
+        }
         
         return redirect()->back();
     }
@@ -32,6 +48,14 @@ class FollowController extends Controller
     public function destroy(User $user)
     {
         auth()->user()->following()->detach($user->id);
+        
+        if (request()->ajax()) {
+            return response()->json([
+                'success' => true,
+                'following' => false,
+                'followers_count' => $user->followers()->count()
+            ]);
+        }
         
         return redirect()->back();
     }
